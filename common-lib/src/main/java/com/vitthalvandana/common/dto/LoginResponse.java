4ada7116a9332f9e3a5904800aca9e42f06cbe34
nyn0391 <50115694+nyn0391@users.noreply.github.com>
2026-04-03 17:15:13 +0200

    Create LoginResponse.java

---
+++ common-lib/src/main/java/com/vitthalvandana/common/dto/LoginResponse.java
@@ -0,0 +1,59 @@
+package com.vitthalvandana.common.dto;
+
+public class LoginResponse {
+    private String token;
+    private String userId;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public LoginResponse() {}
+
+    public LoginResponse(String token, String userId, String email, String firstName, String lastName) {
+        this.token = token;
+        this.userId = userId;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
