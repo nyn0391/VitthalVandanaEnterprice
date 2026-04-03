c4c8b113ce7a0b4e8af2cb767073c4d1384ee6b6
nyn0391 <50115694+nyn0391@users.noreply.github.com>
2026-04-03 15:19:09 +0200

    Adding RegisterRequest.java file

--- user-service/src/main/java/com/vitthalvandana/user/dto/RegisterRequest.java
+++ user-service/src/main/java/com/vitthalvandana/user/dto/RegisterRequest.java
@@ -1,18 +1,14 @@
 package com.vitthalvandana.user.dto;
 
 public class RegisterRequest {
-    public String email;
-    public String firstName;
-    public String lastName;
-    public String password;
-    public String phoneNumber;
-    public String address;
-    public String city;
-    public String state;
-    public String zipCode;
-    public String country;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 
-    public RegisterRequest() {}
+    public RegisterRequest() {
+    }
 
     public String getEmail() {
         return email;
@@ -20,6 +16,14 @@ public class RegisterRequest {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -38,59 +42,11 @@ public class RegisterRequest {
         this.lastName = lastName;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
     }
 }
