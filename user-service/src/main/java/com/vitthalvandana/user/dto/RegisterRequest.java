38875b404f0de690474f3a6183bc9e500228e4d6
nyn0391 <50115694+nyn0391@users.noreply.github.com>
2026-04-03 15:10:40 +0200

    Add RegisterRequest DTO

---
+++ user-service/src/main/java/com/vitthalvandana/user/dto/RegisterRequest.java
@@ -0,0 +1,96 @@
+package com.vitthalvandana.user.dto;
+
+public class RegisterRequest {
+    public String email;
+    public String firstName;
+    public String lastName;
+    public String password;
+    public String phoneNumber;
+    public String address;
+    public String city;
+    public String state;
+    public String zipCode;
+    public String country;
+
+    public RegisterRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
